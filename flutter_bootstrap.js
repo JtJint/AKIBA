f810e5d956c1ac3d875e883381b0f0654e8412c9
JtJint <JtJint@users.noreply.github.com>
2026-02-11 14:54:19 +0000

    deploy: a5eaf985ee7d20f16c301ff70f2025b75fe103ef

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "667891932"
+    serviceWorkerVersion: "2338475983"
   }
 });
