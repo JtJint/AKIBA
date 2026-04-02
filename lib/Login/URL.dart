c162b8084fce70d1208c48542302076091e3d70f
JtJint <jjt9844@naver.com>
2026-04-02 14:44:31 +0900

    loginTest

--- lib/Login/URL.dart
+++ lib/Login/URL.dart
@@ -82,35 +82,35 @@ class _NaverCallbackPageState extends State<NaverCallbackPage> {
         print('statusCode: ${rt.statusCode}');
         print('body: ${rt.body}');
 
-        if (rt.statusCode != 200) {
-          Navigator.of(context).pushReplacementNamed('/login');
-          return;
-        }
+        // if (rt.statusCode != 200) {
+        //   Navigator.of(context).pushReplacementNamed('/login');
+        //   return;
+        // }
 
-        final decodingRt = jsonDecode(rt.body);
+        // final decodingRt = jsonDecode(rt.body);
 
-        final isNewUser = decodingRt['isNewUser'];
+        // final isNewUser = decodingRt['isNewUser'];
 
-        if (isNewUser == true) {
-          html.window.history.replaceState(
-            html.window.history.state,
-            '',
-            '/nickname',
-          );
-          Navigator.of(context).pushReplacementNamed('/nickname');
-        } else if (isNewUser == false) {
-          html.window.history.replaceState(
-            html.window.history.state,
-            '',
-            '/main',
-          );
-          Navigator.of(
-            context,
-          ).pushNamedAndRemoveUntil('/main', (route) => false);
-        } else {
-          html.window.history.replaceState(null, '', '/login');
-          Navigator.of(context).pushReplacementNamed('/login');
-        }
+        // if (isNewUser == true) {
+        //   html.window.history.replaceState(
+        //     html.window.history.state,
+        //     '',
+        //     '/nickname',
+        //   );
+        //   Navigator.of(context).pushReplacementNamed('/nickname');
+        // } else if (isNewUser == false) {
+        //   html.window.history.replaceState(
+        //     html.window.history.state,
+        //     '',
+        //     '/main',
+        //   );
+        //   Navigator.of(
+        //     context,
+        //   ).pushNamedAndRemoveUntil('/main', (route) => false);
+        // } else {
+        //   html.window.history.replaceState(null, '', '/login');
+        //   Navigator.of(context).pushReplacementNamed('/login');
+        // }
       } catch (e, st) {
         print('callback error: $e');
         print(st);
